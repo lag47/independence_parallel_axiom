a6e085e21951b7269a01a6de2016bed12b729918
Lucas Silver <lucsil@seas.upenn.edu>
2023-02-22 15:29:28 -0500

    better notation

--- src/tarski.lean
+++ src/tarski.lean
@@ -2,14 +2,12 @@ import model_theory.basic
 import model_theory.syntax
 import model_theory.language_map
 
-/-
-TODO : notations
--/
 open_locale first_order
 open fin
 namespace first_order
 namespace language
 namespace term
+
 
 
 inductive tarski_relations : ℕ → Type 0
@@ -24,15 +22,20 @@ definition tarski_language : first_order.language :=
     relations := tarski_relations
   }
 
+
+local prefix `'∀s` : 66 := bounded_formula.alls
+local prefix `'∀` : 66 := bounded_formula.all
+--local notation '&\' n' :=
+
 definition point_between' {α} {n : ℕ } 
-  (t1 t2 t3 : tarski_language.term α) :
+  (t1 t2 t3 : tarski_language.term (fin n)) :
   tarski_language.bounded_formula α n :=
     relations.bounded_formula
       tarski_relations.point_between
       ![
-        relabel sum.inl t1,
-        relabel sum.inl t2,
-        relabel sum.inl t3
+        relabel sum.inr t1,
+        relabel sum.inr t2,
+        relabel sum.inr t3
        ]
 
 definition line_cong' {α} {n : ℕ } 
@@ -50,13 +53,13 @@ definition line_cong' {α} {n : ℕ }
 definition fin0 : fin 2 := 0 % 2
 definition fin1 : fin 2 := 1 % 2
 
+local prefix `'&` : 75 := (fun (n : nat) , var n)
 
+local notation  w `⬝` : 70 x `≅` : 70  y `⬝` z : 70 :=
+  line_cong' w x y z
 
-definition tarski_axiom1_open {α}: tarski_language.bounded_formula α 2 :=
-  line_cong' (var fin0) (var fin1) (var fin1) (var fin0)
-
-definition tarski_axiom1 {α} : tarski_language.formula α :=
-  bounded_formula.alls tarski_axiom1_open
+definition tarski_axiom1 {α}: tarski_language.formula α :=
+  ∀' ∀' ((var fin0) ⬝ (var fin1) ≅ (var fin1) ⬝ (var fin0))
 
 end term
 end language
