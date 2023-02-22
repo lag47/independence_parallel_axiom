7a98ba84e10ce14c352f2793ea9a3666e60a4781
Lucas Silver <lucsil@seas.upenn.edu>
2023-02-22 11:56:06 -0500

    formalized tarski axiom 1

--- src/tarski.lean
+++ src/tarski.lean
@@ -1,10 +1,18 @@
 import model_theory.basic
 import model_theory.syntax
+import model_theory.language_map
+
 /-
 TODO : notations
 -/
 open_locale first_order
 open fin
+namespace first_order
+namespace language
+namespace term
+namespace relations
+namespace sum
+
 
 inductive tarski_relations : ℕ → Type 0
   | point_between : tarski_relations 3
@@ -20,27 +28,43 @@ definition tarski_language : first_order.language :=
 
 definition point_between' {α} {n : ℕ } 
   (t1 t2 t3 : tarski_language.term α) :
-  tarski_language.formula α :=
-    first_order.language.relations.bounded_formula
+  tarski_language.bounded_formula α n :=
+    relations.bounded_formula
       tarski_relations.point_between
       ![
-        first_order.language.term.relabel sum.inl t1,
-        first_order.language.term.relabel sum.inl t2,
-        first_order.language.term.relabel sum.inl t3
+        relabel sum.inl t1,
+        relabel sum.inl t2,
+        relabel sum.inl t3
        ]
 
 definition line_cong' {α} {n : ℕ } 
-  (t1 t2 t3 t4 : tarski_language.term α) :
-  tarski_language.formula α :=
-    first_order.language.relations.bounded_formula
+  (t1 t2 t3 t4 : tarski_language.term (fin n)) :
+  tarski_language.bounded_formula α n :=
+    relations.bounded_formula
       tarski_relations.line_cong
       ![
-        first_order.language.term.relabel sum.inl t1,
-        first_order.language.term.relabel sum.inl t2,
-        first_order.language.term.relabel sum.inl t3,
-        first_order.language.term.relabel sum.inl t4
+        relabel sum.inr t1,
+        relabel sum.inr t2,
+        relabel sum.inr t3,
+        relabel sum.inr t4
        ]
 
+definition fin0 : fin 2 := 0 % 2
+definition fin1 : fin 2 := 1 % 2
+
+
+
+definition tarski_axiom1_open {α} : tarski_language.bounded_formula α (fin 2) :=
+  line_cong' (var fin0) (var fin1) (var fin1) (var fin0)
+
+definition tarski_axiom1 : tarski_language.formula (fin 0) :=
+  bounded_formula.alls tarski_axiom1_open
+
+/-
+definition tarski_axiom1 {α} : tarski_language.formula α :=
+ first_order.language.bounded_formula.alls
+  (line_cong' _ _ _ _)
+-/
 /-
 definition line_cong' {α} {n : ℕ } 
   (t1 t2 t3 t4 : tarski_language.term α) :
